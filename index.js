177b4f9753bc588283ff93f437f4ebc72c6b533e
Kali Kiger <kalikiger@gmail.com>
2014-09-21 08:47:17 -0700

    added index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var fishing = function(river, pole){
+  var river = 'great';
+  var pole = 'this awesome pole';
+  var happy = "My fish is bigger than Steve's!";
+  console.log("I'm having the best birthday ever " + 'using ' + pole + " during " + river + " river conditions!");
+  console.log("What makes me happy? " + happy);
+};
+fishing();
+
